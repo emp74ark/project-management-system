a5793b9596d52c21122e27b1eb57bc6ea4b06ef2
emp74ark <andrey.yurkovsky@gmail.com>
2022-10-06 00:15:47 +0200

    feat: basic project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { CommonLayoutComponent } from './shared/components/common-layout/common-layout.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CommonLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: WelcomePageComponent},
+      {path: 'about', component: AboutPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { LoginPageComponent } from './user/login-page/login-page.component';
+import { DashboardPageComponent } from './user/dashboard-page/dashboard-page.component';
+import { ProjectPageComponent } from './user/project-page/project-page.component';
+import { CommonLayoutComponent } from './shared/components/common-layout/common-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    AboutPageComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    ProjectPageComponent,
+    CommonLayoutComponent
   ],
   imports: [
     BrowserModule,
