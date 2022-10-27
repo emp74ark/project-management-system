5f657bd164187391d4b007d9d7a99b85caaeaee8
emp74ark <andrey.yurkovsky@gmail.com>
2022-10-27 12:17:26 +0200

    feat: locale selector style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   imports: [
     HttpClientModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule
   ],
   exports: [
     HttpClientModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule
   ],
   declarations: [
   ]
